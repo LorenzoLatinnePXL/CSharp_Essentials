1bd5935173d6eadd6f15ac12da53667c42dbedf8
LorenzoLatinnePXL <lorenzo.latinne@student.pxl.be>
2024-11-17 16:09:36 +0100

    add toon-inkomen

---
+++ 5_Methodes/methods-2-toon-inkomen-LorenzoLatinnePXL/README.md
@@ -0,0 +1,12 @@
+# Oefening 2: ToonInkomen
+
+Schrijf een methode *ShowIncome()* met 2 integer parameters die het
+totale inkomen over alle jaren berekend. Aangenomen wordt dat er elk
+jaar evenveel verdiend wordt.
+
+Als bijkomende uitbreidingen kan je ook een functie-methode (geeft een
+waarde terug) schrijven, een methode met de parameter out en een methode met een ref parameter.
+
+![Afbeelding met tekst, schermopname, scherm, software Automatisch
+gegenereerde
+beschrijving](./media/image1.png)
---
+++ 5_Methodes/methods-2-toon-inkomen-LorenzoLatinnePXL/ToonInkomen/ToonInkomen/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ToonInkomen
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void startButton_Click(object sender, RoutedEventArgs e)
+        {
+            bool isValidIncome = double.TryParse(yearlyTextBox.Text, out double yearSalary);
+            bool isValidYears = int.TryParse(yearsTextBox.Text, out int years);
+            double calculation = 0;
+
+            if (!isValidIncome || !isValidYears)
+            {
+                salaryTextBox.Text = "Invalid input";
+            }
+            else
+            {
+                salaryTextBox.Text = ShowIncome(yearSalary, years, out calculation);
+            }
+            Console.WriteLine(calculation);
+        }
+
+        private string ShowIncome(double yearSalary, int years, out double calculation)
+        {
+            calculation = yearSalary * years;
+            return "€ " + (yearSalary * years).ToString("N2");
+        }
+    }
+}
